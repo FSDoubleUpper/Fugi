b8b04dc0b8a27dd7f63a76f02c87aea801bd5866
Yao Lee <fsdoubleupper@gmail.com>
2023-04-14 16:50:22 -0700

    about update2

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,25 +23,15 @@ const About = (props) => {
             <section className="about-introduction">
                 <div className="about-intro2">
                     <div className="about-intro2-img">
-                        <figcaption>
-                            <img
-                                src="./images/stack.jpg"
-                                alt="a boy stacking bricks"
-                                className="about-me brickboy"
-                            ></img>
-                            Photo by{" "}
-                            <a href="https://unsplash.com/@bel2000a?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                                Belinda Fewings
-                            </a>{" "}
-                            on{" "}
-                            <a href="https://unsplash.com/photos/6p-KtXCBGNw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                                Unsplash
-                            </a>
-                        </figcaption>
+                        <img
+                            src="./images/stack.jpg"
+                            alt="a boy stacking bricks"
+                            className="about-me brickboy"
+                        ></img>
                     </div>
                     <div>
                         <p>
-                            I'm Yao and I'm a Full Stack Developer from
+                            My name is Yao and I'm a Full Stack Developer from
                             California.
                         </p>
                     </div>
@@ -49,49 +39,60 @@ const About = (props) => {
                 <div className="about-intro3">
                     <div>
                         <p>
-                            &emsp;I have a passion for programming as it allows
-                            me to take the ideas swirling around in my head and
-                            turn them into something real.
+                            &emsp;I have a strong passion for programming as it
+                            allows me to take the ideas swirling around in my
+                            head and turn them into something real. It's like a
+                            superpower I wish I had... maybe... maybe one day...
                             <br />
                             <br />
                             Whether it's a new app or a website, programming
-                            gives me the tools to bring my visions to life and
-                            share them with the world.
+                            brings me joy and helps me bring my visions to life
+                            so I can share them with the world.
                         </p>
                     </div>
                     <div className="about-intro3-img">
-                        <figcaption>
-                            <img
-                                src="./images/laptop.jpg"
-                                alt="laptop balanced on a fingertip"
-                                className="about-me"
-                            ></img>
-                            Photo by{" "}
-                            <a href="https://unsplash.com/@pakata?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                                Pakata Goh
-                            </a>{" "}
-                            on{" "}
-                            <a href="https://unsplash.com/photos/RDolnHtjVCY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                                Unsplash
-                            </a>
-                        </figcaption>
+                        <img
+                            src="./images/laptop.jpg"
+                            alt="laptop balanced on a fingertip"
+                            className="about-me"
+                        ></img>
+                    </div>
+                </div>
+                <div className="about-intro4">
+                    <div className="about-intro4-img">
+                        <img
+                            src="./images/me-photo.jpg"
+                            alt="self portrait of Yao Lee taking photo of himself"
+                            className="about-me meCamera"
+                        ></img>
+                    </div>
+                    <div>
+                        <p>
+                            However when I'm not busy bringing my code to life,
+                            you can find me behind the lens of a camera...
+                        </p>
+                    </div>
+                </div>
+                <div className="about-intro5">
+                    <div>
+                        <p>...Or indulging in some video game action!</p>
+                    </div>
+                    <div className="about-intro5-img">
+                        <img
+                            src="./images/gamemania.jpg"
+                            alt="man infront of a game store"
+                            className="about-me"
+                        ></img>
                     </div>
                 </div>
                 <div>
-                    <p>
-                        However when I'm not busy bringing my code to life, you
-                        can find me behind the lens of a camera...
-                    </p>
-                </div>
-                <div>
-                    <p>...Or indulging in some video game action!</p>
-                </div>
-                <div>
-                    <p>
-                        So if you're in need of some top-notch code or just want
-                        to chat about the latest and greatest in the tech world,
-                        don't hesitate to reach out!
-                    </p>
+                    <div>
+                        <p>
+                            So if you're in need of some top-notch code or just
+                            want to chat about the latest and greatest in the
+                            tech world, don't hesitate to reach out!
+                        </p>
+                    </div>
                 </div>
             </section>
         </main>
