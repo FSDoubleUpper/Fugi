8091f5af58ff23ed30550ba2ddaf44dbe5de076a
Yao Lee <65222173+FSDoubleUpper@users.noreply.github.com>
2023-05-10 10:14:30 -0700

    Semantic update on FEColorz

--- src/pages/FEColorz.js
+++ src/pages/FEColorz.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const FEColorz = (props) => {
     return (
-        <div className="main">
-            <h1>FEColorz</h1>
-            <div className="fecolorz-info">
+        <main className="main">
+            <header>
+                <h1>FEColorz</h1>
+            </header>
+            <section className="fecolorz-info">
                 <img
                     className="fecolorz-screenshot"
                     src="images\fecolorz.png"
@@ -19,29 +21,31 @@ const FEColorz = (props) => {
                         CLICK HERE to USE
                     </a>
                 </div>
-                <p>
-                    fecolorz is a web app that helps front-end developers who
-                    have road blocks on font color and background color
-                    combination.
+                <summary>
+                    <p>
+                        fecolorz is a web app that helps front-end developers
+                        who have road blocks on font color and background color
+                        combination.
+                        <br />
+                        fecolorz assist by generating random colors for font and
+                        background as well as allowing an editable custom text
+                        for better visualization.
+                        <br />
+                    </p>
                     <br />
-                    fecolorz assist by generating random colors for font and
-                    background as well as allowing an editable custom text for
-                    better visualization.
                     <br />
-                </p>
-                <br />
-                <br />
-                <p>features for:</p>
-                <ul className="fecolorz-list">
-                    <li>custom text edit</li>
-                    <li>randomize color font text</li>
-                    <li>randomize color for background</li>
-                    <li>provide RGB code</li>
-                    <li>provide Hex code</li>
-                    <li>easily click on code to copy to clipboard</li>
-                </ul>
-            </div>
-        </div>
+                    <p>features for:</p>
+                    <ul className="fecolorz-list">
+                        <li>custom text edit</li>
+                        <li>randomize color font text</li>
+                        <li>randomize color for background</li>
+                        <li>provide RGB code</li>
+                        <li>provide Hex code</li>
+                        <li>easily click on code to copy to clipboard</li>
+                    </ul>
+                </summary>
+            </section>
+        </main>
     );
 };
 
